090bf63e23650187282d92f227587ce24db0d6cf
Justin Etzine <justetz@gmail.com>
2015-02-16 22:32:32 -0500

    added .gitignore

--- QuorumCalculator.cpp
+++ QuorumCalculator.cpp
@@ -1,18 +1,22 @@
 
+typedef std::vector<std::vector<std::string>> STR_DOUBLE_VEC;
+
 //definitons
 void get_next_line(std::istream& in_str, std::vector<std::string>& contents);
 
 //main
 int main(int argc, char* argc[]) {
-	if
+	STR_DOUBLE_VEC contents;
 }
 
 //functions
-void get_next_line(std::istream& in_str, std::vector<std::string>& contents) {
+void get_next_line(std::istream& in_str, STR_DOUBLE_VEC& contents) {
     std::string line, cell;
     std::getline(in_str, line);
 
     std::stringstream line_stream(line);
+    std::vector t;
+    contents.push_back(t);
 
     while(std::getline(lineStream,cell,','))
         contents.push_back(cell);
